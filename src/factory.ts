af7581d3e31e8ae91a60f9b13ea3223df1b7e9f0
René Kulik <rene@kulik.io>
2024-11-07 15:52:21 +0100

    feat: make config usage customizable

--- src/factory.ts
+++ src/factory.ts
@@ -1,7 +1,28 @@
 import { destructuring, imports, javascript, preferArrow, prettier, typescript } from './configs';
-import type { Config } from './types';
+import type { Config, OptionsConfig } from './types';
 import { combineConfigs } from './utils';
 
-export const rkulik = (...userConfigs: (Config | Config[])[]): Config[] => {
-  return combineConfigs(destructuring, imports, javascript, preferArrow, prettier, typescript, ...userConfigs);
+export const rkulik = (options: OptionsConfig = {}, ...userConfigs: (Config | Config[])[]): Config[] => {
+  const {
+    destructuring: includeDestructuring = true,
+    imports: includeImports = true,
+    javascript: includeJavasScript = true,
+    preferArrow: includePreferArrow = true,
+    prettier: includePrettier = true,
+    typescript: includeTypeScript = true,
+  } = options;
+
+  const configs: (Config | Config[])[] = [
+    includeDestructuring ? destructuring : [],
+    includeImports ? imports : [],
+    includeJavasScript ? javascript : [],
+    includePreferArrow ? preferArrow : [],
+    includePrettier ? prettier : [],
+    includeTypeScript ? typescript : [],
+    ...userConfigs,
+  ].filter(configItem => {
+    return !Array.isArray(configItem) || !!configItem.length;
+  });
+
+  return combineConfigs(...configs);
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,12 @@
 import type { FlatConfig } from '@typescript-eslint/utils/ts-eslint';
 
 export type Config = FlatConfig.Config;
+
+export interface OptionsConfig {
+  destructuring?: boolean;
+  imports?: boolean;
+  javascript?: boolean;
+  preferArrow?: boolean;
+  prettier?: boolean;
+  typescript?: boolean;
+}
