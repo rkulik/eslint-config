37c996d980e944402d49a7eee2ddacd2b4512595
René Kulik <rene@kulik.io>
2024-11-12 15:43:26 +0100

    feat: add default export for main function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { rkulik } from './factory';
+
 export * as configs from './configs';
-export { rkulik } from './factory';
 export * as utils from './utils';
+
+// eslint-disable-next-line import/no-default-export
+export default rkulik;
