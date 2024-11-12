06fe9b0714df949bd4eaf16ad6afb1f1f3b3d35f
René Kulik <rene@kulik.io>
2024-11-12 16:55:38 +0100

    test: add factory tests

---
+++ tests/factory.test.ts
@@ -0,0 +1,76 @@
+import { describe, expect, jest, test } from '@jest/globals';
+import { isPackageExists } from 'local-pkg';
+
+import rkulik from '../src';
+import {
+  destructuring,
+  imports,
+  javascript,
+  preferArrow,
+  prettier,
+  react,
+  tailwindcss,
+  typescript,
+} from '../src/configs';
+
+jest.mock('local-pkg', () => {
+  return {
+    isPackageExists: jest.fn(),
+  };
+});
+
+const mockedIsPackageExists = isPackageExists as jest.MockedFunction<typeof isPackageExists>;
+
+describe('factory', () => {
+  describe('rkulik', () => {
+    test('should return default configs', () => {
+      mockedIsPackageExists.mockReturnValue(true);
+      expect(rkulik()).toEqual([
+        ...destructuring,
+        ...imports,
+        ...javascript,
+        ...preferArrow,
+        ...prettier,
+        ...react,
+        ...tailwindcss,
+        ...typescript,
+      ]);
+    });
+
+    test('should include user configs', () => {
+      mockedIsPackageExists.mockReturnValue(true);
+      expect(rkulik({}, { ignores: ['**/dist'] })).toEqual([
+        ...destructuring,
+        ...imports,
+        ...javascript,
+        ...preferArrow,
+        ...prettier,
+        ...react,
+        ...tailwindcss,
+        ...typescript,
+        { ignores: ['**/dist'] },
+      ]);
+    });
+
+    test('should include configs based on installed packages', () => {
+      mockedIsPackageExists.mockReturnValue(false);
+      expect(rkulik()).toEqual([...destructuring, ...imports, ...javascript, ...preferArrow, ...prettier]);
+    });
+
+    test('should include configs based on options', () => {
+      mockedIsPackageExists.mockReturnValue(false);
+      expect(
+        rkulik({
+          destructuring: false,
+          imports: false,
+          javascript: false,
+          preferArrow: false,
+          prettier: false,
+          react: true,
+          tailwindcss: true,
+          typescript: true,
+        }),
+      ).toEqual([...react, ...tailwindcss, ...typescript]);
+    });
+  });
+});
