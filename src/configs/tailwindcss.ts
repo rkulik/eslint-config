102887c6290afcbcdcdeb96d9dcebea73996a69d
René Kulik <rene@kulik.io>
2024-12-13 12:44:33 +0100

    refactor: use proper noun for tailwind css plugin

--- src/configs/tailwindcss.ts
+++ src/configs/tailwindcss.ts
@@ -1,13 +1,13 @@
-import { pluginTailwindCss } from '../plugins';
+import { pluginTailwindCSS } from '../plugins';
 import type { Config } from '../types';
 
 export const tailwindcss: Config[] = [
   {
     plugins: {
-      tailwindcss: pluginTailwindCss,
+      tailwindcss: pluginTailwindCSS,
     },
     rules: {
-      ...pluginTailwindCss.configs.recommended.rules,
+      ...pluginTailwindCSS.configs.recommended.rules,
     },
   },
 ];
