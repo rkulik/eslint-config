50373a859d6e0c10fa3d85b7b2b27728d5f5d065
René Kulik <rene@kulik.io>
2025-01-03 20:23:25 +0100

    feat: ignore package-lock.json by default

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -2,6 +2,6 @@ import type { Config } from '../types';
 
 export const ignores: Config[] = [
   {
-    ignores: ['**/dist', '**/build'],
+    ignores: ['**/build', '**/dist', '**/package-lock.json'],
   },
 ];
